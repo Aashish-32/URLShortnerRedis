015f8b5d777e32adb494cf87345e3276c815dc6c
Aashish <aaxishpunk31@gmail.com>
2024-05-11 18:46:16 +0545

    redisConn

--- api/database/database.go
+++ api/database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"context"
+	"os"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -10,9 +11,8 @@ var Ctx = context.Background()
 
 func CreateClient(dbno int) *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
-		DB:       dbno,
+		Addr: os.Getenv("DB_ADDR"),
+		DB:   dbno,
 	})
 	return rdb
 }
